179d8607519aeff91f770812199d96469ee03b1d
altguerrero <josealberto.guerreroayala@gmail.com>
2020-12-07 03:17:31 -0500

    Integrated components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,24 @@ module.exports = {
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      center: true,
+    },
+    colors: {
+      primary: {
+        light: "#F1B95B",
+        dark: "#F0B24A",
+      },
+      secondary: {
+        normal: "#606060",
+        light: "#707070",
+      },
+      white: "#FFF",
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
